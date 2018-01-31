2334efad8eab388a267678d781383ac8e4a52666
extrazeldev <extrazeldev@gmail.com>
2018-01-30 23:53:10 -0700

    Added seeds

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -121,6 +121,8 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
         vSeeds.push_back(CDNSSeedData("zelcash.tech", "dnsseed.zelcash.tech")); // Zelcash
+        vSeeds.push_back(CDNSSeedData("node.zel.cash", "seed.zel.cash")); // Zelcash
+        vSeeds.push_back(CDNSSeedData("node1.zel.cash", "dns.zel.cash")); // Zelcash
 
         // guarantees the first 2 characters, when base58 encoded, are "t1"
         base58Prefixes[PUBKEY_ADDRESS]     = {0x1C,0xB8};
